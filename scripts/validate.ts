0b887fd64dbf05130f3e7cfd1698dc44d4c1d41b
tony <tony.li@vechain.com>
2019-03-05 16:32:59 +0800

    validate script

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -1,5 +1,63 @@
+import * as fs from 'fs'
+import { promisify } from 'util'
+import * as path from 'path'
+    
+const bundleName = /^([a-z0-9\-]+\.){2}[a-z0-9\-]+$/
+const url = /^(http(s?):\/\/)([a-zA-Z0-9.-]+)(:[0-9]{1,4})?/
+let appCount = 0;
 
+const colors = {
+    red(str: string) {
+        return `\x1b[31m${str}\x1b[0m`
+    },
+    green(str: string) {
+        return `\x1b[32m${str}\x1b[0m`
+    },
+}
 
-console.log('Validation passed, Congrats!')
+const ensure = (condition: boolean, msg = 'assert error') => {
+    if (!condition) {
+        throw new Error(msg) 
+    }
+}
 
-process.exit(0)
+const checkAPP = async (dir: fs.Dirent) => {
+    if (!bundleName.test(dir.name)) {
+        throw new Error(`invalid app bundle id: ${dir.name}`)
+    }
+    let files = await promisify(fs.readdir)(path.join(__dirname, '../apps', dir.name))
+    ensure(files.includes('manifest.json'), 'manifest.json is required')
+    ensure(files.includes('logo.png'), 'logo.png is required')
+    ensure(files.length == 2, 'only logo.png and manifest.json allowed')
+
+    const manifest = require(path.join(__dirname, '../apps', dir.name, 'manifest.json'))
+    ensure(manifest.name && typeof manifest.name === 'string' && manifest.name.length, 'name should be a string')
+    ensure(manifest.href && url.test(manifest.href), 'href should be a url')
+    ensure(manifest.desc && typeof manifest.desc === 'string' && manifest.desc.length, 'desc should be a string')
+    ensure(Array.isArray(manifest.tags), 'tags should be an array')
+
+    manifest.tags.forEach((tag:string) => {
+        ensure(!!tag && !!tag.length, 'tags should be a string')
+    });
+}
+    
+; (async () => {
+    let dirs = await promisify(fs.readdir)(path.join(__dirname, '../apps'), { withFileTypes: true })
+    for (let dir of dirs) {
+        if (dir.isDirectory()) {
+            await checkAPP(dir).catch(e=>{throw new Error(`check ${dir.name} -> ${e.message}`)})
+            appCount++
+        } else {
+            if (dir.name != '.gitkeep') {
+                throw new Error('invalid file in apps dir: '+ dir.name)
+            }
+        }
+    }
+})().catch(e => {
+    console.log(colors.red('Validation failed: '+ e.message))
+    process.exit(1)
+}).then(() => {
+    console.log(colors.green(`Validation passed, processed ${appCount} apps. Congrats!`))
+    process.exit(0)
+})
+
