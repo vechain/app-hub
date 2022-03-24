28d45967526c5420f8fe1ac3d030b0445d6537e1
tony <liboliqi@gmail.com>
2022-03-24 14:27:09 +0800

    improve scripts

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import { promisify } from 'util'
 import * as path from 'path'
 import imageSize from 'image-size'
-    
+
 const bundleName = /^(([a-z0-9\-]+\.)+)[a-z0-9\-]+$/
 const url = /^(http(s?):\/\/)([a-zA-Z0-9.-]+)(:[0-9]{1,4})?/
 let appCount = 0;
@@ -18,7 +18,7 @@ const colors = {
 
 const ensure = (condition: boolean, msg = 'assert error') => {
     if (!condition) {
-        throw new Error(msg) 
+        throw new Error(msg)
     }
 }
 
@@ -26,20 +26,27 @@ const checkAPP = async (dir: fs.Dirent) => {
     if (!bundleName.test(dir.name)) {
         throw new Error(`invalid app bundle id: ${dir.name}`)
     }
+
+    const required = ['manifest.json', 'logo.png']
     let files = await promisify(fs.readdir)(path.join(__dirname, '../apps', dir.name), { withFileTypes: true })
+    const fNames: string[] = []
     for (let file of files) {
-        ensure(file.isFile(), 'app directory must contain only file')
-        if (file.name === 'manifest.json' || file.name === 'logo.png')
+        ensure(file.isFile(), 'folders are not allowed in app directory')
+        if (file.name === '.DS_Store') {
+            ensure(process.env.CI !== 'true', '.DS_Store is not allowed')
             continue
-        if (file.name == '.DS_Store' && process.env.CI !== 'true')
-            continue
-        throw new Error('only logo.png and manifest.json allowed')
+        }
+        ensure(required.includes(file.name), file.name+' is not allowed')
+        fNames.push(file.name)
     }
-    ensure(files.length >= 2, 'logo.png and manifest.json are both required')
+
+    for (const name of required) {
+        ensure(fNames.includes(name), name+' is required')
+    }
 
     const dimensions = await promisify(imageSize)(path.join(__dirname, '../apps', dir.name, 'logo.png'));
-    ensure(!!dimensions&&dimensions.type === 'png', 'logo should be a png file')
-    ensure(!!dimensions && dimensions.height === 512 && dimensions.width===512, 'logo should be 512x512 in pixel size')
+    ensure(!!dimensions && dimensions.type === 'png', 'logo should be image file in png format')
+    ensure(!!dimensions && dimensions.height === 512 && dimensions.width === 512, 'logo should be 512x512 in pixel size')
 
     const manifest = require(path.join(__dirname, '../apps', dir.name, 'manifest.json'))
     ensure(manifest.name && typeof manifest.name === 'string' && manifest.name.length, 'name should be a string')
@@ -47,16 +54,16 @@ const checkAPP = async (dir: fs.Dirent) => {
     ensure(manifest.desc && typeof manifest.desc === 'string' && manifest.desc.length, 'desc should be a string')
     ensure(Array.isArray(manifest.tags), 'tags should be an array')
 
-    manifest.tags.forEach((tag:string) => {
+    manifest.tags.forEach((tag: string) => {
         ensure(!!tag && !!tag.length, 'tags should be a string')
     });
 }
-    
+
 ; (async () => {
     let dirs = await promisify(fs.readdir)(path.join(__dirname, '../apps'), { withFileTypes: true })
     for (let dir of dirs) {
         if (dir.isDirectory()) {
-            await checkAPP(dir).catch(e=>{throw new Error(`check ${dir.name} -> ${e.message}`)})
+            await checkAPP(dir).catch(e => { throw new Error(`check ${dir.name} -> ${e.message}`) })
             appCount++
         } else {
             if (dir.name === '.gitkeep')
@@ -67,7 +74,7 @@ const checkAPP = async (dir: fs.Dirent) => {
         }
     }
 })().catch(e => {
-    console.log(colors.red('Validation failed: '+ e.message))
+    console.log(colors.red('Validation failed: ' + e.message))
     process.exit(1)
 }).then(() => {
     console.log(colors.green(`Validation passed, processed ${appCount} apps. Congrats!`))
