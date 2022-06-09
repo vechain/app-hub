7a437dd6fb630514cd6b642c82ae41cc1e9e120f
tony <liboliqi@gmail.com>
2022-06-09 13:16:29 +0800

    improve validate script

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -7,7 +7,6 @@ const bundleName = /^(([a-z0-9\-]+\.)+)[a-z0-9\-]+$/
 const url = /^(http(s?):\/\/)([a-zA-Z0-9.-]+)(:[0-9]{1,4})?/
 const address = /^0x[a-f0-9]{40}$/
 const category = /collectibles|defi|games|marketplaces|utilities/
-let appCount = 0;
 
 const colors = {
     red(str: string) {
@@ -38,12 +37,12 @@ const checkAPP = async (dir: fs.Dirent) => {
             ensure(process.env.CI !== 'true', '.DS_Store is not allowed')
             continue
         }
-        ensure(required.includes(file.name), file.name+' is not allowed')
+        ensure(required.includes(file.name), file.name + ' is not allowed')
         fNames.push(file.name)
     }
 
     for (const name of required) {
-        ensure(fNames.includes(name), name+' is required')
+        ensure(fNames.includes(name), name + ' is required')
     }
 
     const dimensions = await promisify(imageSize)(path.join(__dirname, '../apps', dir.name, 'logo.png'));
@@ -51,27 +50,27 @@ const checkAPP = async (dir: fs.Dirent) => {
     ensure(!!dimensions && dimensions.height === 512 && dimensions.width === 512, 'logo should be 512x512 in pixel size')
 
     const manifest = require(path.join(__dirname, '../apps', dir.name, 'manifest.json'))
-    ensure(manifest.name && typeof manifest.name === 'string' && manifest.name.length, 'name should be a string')
-    ensure(manifest.href && url.test(manifest.href), 'href should be a url and start with http or https')
-    ensure(manifest.desc && typeof manifest.desc === 'string' && manifest.desc.length, 'desc should be a string')
-    ensure(manifest.category && typeof manifest.category === 'string' && manifest.category.length && category.test(manifest.category), 'invalid category')
+    ensure(manifest.name && typeof manifest.name === 'string', 'name should be a string')
+    ensure(manifest.href && typeof manifest.href === 'string' && url.test(manifest.href), 'href should be a url and start with http or https')
+    ensure(manifest.desc && typeof manifest.desc === 'string', 'desc should be a string')
+    ensure(manifest.category && typeof manifest.category === 'string' && category.test(manifest.category), 'invalid category')
     ensure(Array.isArray(manifest.tags), 'tags should be an array')
     if (manifest.repo) {
-        ensure(manifest.repo && url.test(manifest.repo), 'repo should be a url and start with http or https')
+        ensure(manifest.repo && typeof manifest.repo === 'string' && url.test(manifest.repo), 'repo should be a url and start with http or https')
     }
     if (manifest.contracts) {
         ensure(Array.isArray(manifest.contracts), 'contracts should be an array')
-        manifest.contracts.forEach((contracts: string) => {
-            ensure(!!contracts && !!address.test(contracts), 'invalid contract address')
-        });
+        manifest.contracts.forEach((contract: any) => {
+            ensure(contract && typeof contract === 'string'&& address.test(contract), 'invalid contract address')
+        })
     }
     manifest.tags.forEach((tag: string) => {
         ensure(!!tag && !!tag.length, 'tags should be a string')
     });
-
 }
 
-; (async () => {
+let appCount = 0
+void (async () => {
     let dirs = await promisify(fs.readdir)(path.join(__dirname, '../apps'), { withFileTypes: true })
     for (let dir of dirs) {
         if (dir.isDirectory()) {
@@ -84,12 +83,12 @@ const checkAPP = async (dir: fs.Dirent) => {
                 continue
             throw new Error('invalid file in apps dir: ' + dir.name)
         }
-        }
-    })().catch(e => {
-        console.log(colors.red('Validation failed: ' + e.message))
-        process.exit(1)
-    }).then(() => {
-        console.log(colors.green(`Validation passed, processed ${appCount} apps. Congrats!`))
-        process.exit(0)
-    })
+    }
+})().catch(e => {
+    console.log(colors.red('Validation failed: ' + e.message))
+    process.exit(1)
+}).then(() => {
+    console.log(colors.green(`Validation passed, processed ${appCount} apps. Congrats!`))
+    process.exit(0)
+})
 
